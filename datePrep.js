b58795962686115ed27ad1e74782fd96d9f8706b
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-25 09:40:52 -0400

    Update datePrep.js

--- datePrep.js
+++ datePrep.js
@@ -1,6 +1,4 @@
-/* Used to prepolulate "Date" fields with today's date.
-   See Citation 1, 2, & 3 below.
-*/
+/* Used to prepolulate "Date" fields with today's date. */
 function getTDate(){
 	var date = new Date();
 	var day = date.getDate();
@@ -12,8 +10,3 @@ function getTDate(){
 	var today = String(year + "-" + month + "-" + day);
 	return today;	
 }
-/* Citations.
-[1] https://piazza.com/class/ipg9pgicbxl5mk?cid=129 (Summer 2016 CS-290)
-[2] http://stackoverflow.com/questions/6982692/html5-input-type-date-default-value-to-today
-[3] http://www.w3schools.com/jsref/jsref_string.asp
-*/
